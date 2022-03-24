80327a3fbd4e4d489470e32068542ec0aec74ee1
Viriyadhi <viriyadhitjan@gmail.com>
2022-03-24 18:06:04 +0700

    feat(model)[VS]: initializing model for user and addresses

--- models/about.js
+++ models/about.js
@@ -1,25 +1,23 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class about extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class about extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
     }
-  }
-  about.init({
-    desc_1: DataTypes.STRING,
-    desc_2: DataTypes.STRING,
-    desc_3: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'about',
-  });
-  return about;
+    about.init({
+        desc_1: DataTypes.STRING,
+        desc_2: DataTypes.STRING,
+        desc_3: DataTypes.STRING,
+    }, {
+        sequelize,
+        modelName: "about",
+    });
+    return about;
 };
--- models/addresses.js
+++ models/addresses.js
@@ -1,30 +1,28 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class addresses extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class addresses extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
     }
-  }
-  addresses.init({
-    provinsi: DataTypes.STRING,
-    kota: DataTypes.STRING,
-    kecamatan: DataTypes.STRING,
-    kelurahan_desa: DataTypes.STRING,
-    jalan: DataTypes.STRING,
-    kode_pos: DataTypes.INTEGER,
-    nama_alamat: DataTypes.STRING,
-    user_id: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'addresses',
-  });
-  return addresses;
+    addresses.init({
+        provinsi: DataTypes.STRING,
+        kota: DataTypes.STRING,
+        kecamatan: DataTypes.STRING,
+        kelurahan_desa: DataTypes.STRING,
+        jalan: DataTypes.STRING,
+        kode_pos: DataTypes.INTEGER,
+        nama_alamat: DataTypes.STRING,
+        user_id: DataTypes.INTEGER,
+    }, {
+        sequelize,
+        modelName: "addresses",
+    });
+    return addresses;
 };
