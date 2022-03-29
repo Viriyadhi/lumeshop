4a4ef4b7e98ab26cb33dd5255f5b704053b6db74
Viriyadhi <viriyadhitjan@gmail.com>
2022-03-29 10:27:04 +0700

    fix(relationship)[VS]: fixing data relationship

--- migrations/20220322134006-create-cart.js
+++ migrations/20220322134006-create-cart.js
@@ -1,30 +1,40 @@
-'use strict';
+"use strict";
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('carts', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      products_id: {
-        type: Sequelize.INTEGER
-      },
-      user_id: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('carts');
-  }
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable("carts", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            products_id: {
+                type: Sequelize.INTEGER,
+                foreignKey: true,
+                references: {
+                    model: "products",
+                    key: "id",
+                },
+            },
+            user_id: {
+                type: Sequelize.INTEGER,
+                foreignKey: true,
+                references: {
+                    model: "users",
+                    key: "id",
+                },
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable("carts");
+    },
 };
--- migrations/20220322135852-create-about.js
+++ migrations/20220322135852-create-about.js
@@ -1,33 +1,27 @@
-'use strict';
+"use strict";
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('abouts', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      desc_1: {
-        type: Sequelize.STRING
-      },
-      desc_2: {
-        type: Sequelize.STRING
-      },
-      desc_3: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('abouts');
-  }
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable("about", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            desc_1: {
+                type: Sequelize.STRING,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable("about");
+    },
 };
