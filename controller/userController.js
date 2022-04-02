a44325b512a9885d5d7368dd7c9a33b0d16c843c
Viriyadhi <viriyadhitjan@gmail.com>
2022-04-02 17:29:45 +0700

    feat(validate user)[VS] validating user when registration (email, password)

--- controller/userController.js
+++ controller/userController.js
@@ -1,123 +1,108 @@
 const { user } = require("../models");
-
-// exports.name = (req, res) => {
-//     user
-//         .findAll({
-//             attributes: ["name"],
-//             where: {
-//                 id: req.params.id,
-//             },
-//         })
-//         .then((data) => {
-//             res.send(data);
-//         });
-// };
-
-// exports.email = (req, res) => {
-//     user
-//         .findAll({
-//             attributes: ["email"],
-//             where: {
-//                 id: req.params.id,
-//             },
-//         })
-//         .then((data) => {
-//             res.send(data);
-//         });
-// };
-
-// exports.password = (req, res) => {
-//     user
-//         .findAll({
-//             attributes: ["password"],
-//             where: {
-//                 id: req.params.id,
-//             },
-//         })
-//         .then((data) => {
-//             res.send(data);
-//         });
-// };
-
-// exports.username = (req, res) => {
-//     user
-//         .findAll({
-//             attributes: ["username"],
-//             where: {
-//                 id: req.params.id,
-//             },
-//         })
-//         .then((data) => {
-//             res.send(data);
-//         });
-// };
-
-// exports.birth_date = (req, res) => {
-//     user
-//         .findAll({
-//             attributes: ["birth_date"],
-//             where: {
-//                 id: req.params.id,
-//             },
-//         })
-//         .then((data) => {
-//             res.send(data);
-//         });
-// };
-
-// exports.phone_number = (req, res) => {
-//     user
-//         .findAll({
-//             attributes: ["phone_number"],
-//             where: {
-//                 id: req.params.id,
-//             },
-//         })
-//         .then((data) => {
-//             res.send(data);
-//         });
-// };
+const bcrypt = require("bcryptjs");
 
 exports.all = (req, res) => {
-    user.findAll().then((data) => {
-        res.send(data);
-    });
+    user
+        .findAll({
+            attributes: {
+                exclude: ["password"],
+            },
+        })
+        .then((data) => {
+            res.send(data);
+        });
 };
 
 exports.findById = (req, res) => {
-    user.findByPk(req.params.id).then((data) => {
-        if (data) {
-            res.send(data);
-        } else
-            res
-            .status(400)
-            .json({ msg: `No address with the id of ${req.params.id}` });
-    });
+    user
+        .findByPk(req.params.id, {
+            attributes: {
+                exclude: ["password"],
+            },
+        })
+        .then((data) => {
+            if (data) {
+                res.send(data);
+            } else
+                res
+                .status(400)
+                .json({ msg: `No address with the id of ${req.params.id}` });
+        });
 };
 
 exports.create = (req, res) => {
-    const newUser = {
-        name: req.fields.name,
-        email: req.fields.email,
-        password: req.fields.password,
-        username: req.fields.username,
-        birth_date: req.fields.birth_date,
-        phone_number: req.fields.phone_number,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-    };
-    if (!newUser.name ||
-        !newUser.email ||
-        !newUser.password ||
-        !newUser.username ||
-        !newUser.birth_date ||
-        !newUser.phone_number
-    ) {
-        return res.status(400).json({ msg: "Please fill all the requirements" });
-    }
-    user.create(newUser).then((data) => {
-        res.send(data);
-    });
+    user
+        .findOne({
+            where: {
+                email: req.fields.email,
+            },
+        })
+        .then((data) => {
+            if (data) {
+                return res
+                    .status(400)
+                    .json({ msg: `Email ${req.fields.email} already in use` });
+            } else {
+                const validateEmail = (email) => {
+                    return String(email)
+                        .toLowerCase()
+                        .match(
+                            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"] +)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                        );
+                };
+                const validatePassword = (password) => {
+                    return String(password).match(
+                        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/
+                    );
+                };
+
+                if (validateEmail(req.fields.email)) {
+                    bcrypt.hash(req.fields.password, 10, (err, hash) => {
+                        if (err) {
+                            return res.status(500).send({
+                                msg: err,
+                            });
+                        } else {
+                            const newUser = {
+                                name: req.fields.name,
+                                email: req.fields.email,
+                                password: hash,
+                                username: req.fields.username,
+                                birth_date: req.fields.birth_date,
+                                phone_number: req.fields.phone_number,
+                                createdAt: new Date(),
+                                updatedAt: new Date(),
+                            };
+                            if (!newUser.name ||
+                                !newUser.email ||
+                                !newUser.password ||
+                                !newUser.username ||
+                                !newUser.birth_date ||
+                                !newUser.phone_number
+                            ) {
+                                return res
+                                    .status(400)
+                                    .json({ msg: "Please fill all the requirements" });
+                            }
+                            user.create(newUser).then((data) => {
+                                res.status(201).send({
+                                    msg: "The user has been registerd !",
+                                });
+                            });
+                        }
+                    });
+                } else {
+                    return res
+                        .status(400)
+                        .json({ msg: "Please input a valid email structure" });
+                }
+                if (!validatePassword(req.fields.password)) {
+                    return res.status(400).json({
+                        msg: "The password must at least 6 characters long, and must have at least  1 uppercase, and 1 number, and 1 lowercase",
+                    });
+                }
+            }
+        });
 };
 
 exports.update = (req, res) => {
